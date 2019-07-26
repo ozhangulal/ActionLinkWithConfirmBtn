b9998b18f51122e4d4369754dc6226cd6683ec54
ozhangulal <ozhangulal@outlook.com>
2019-07-26 17:10:34 +0300

    Delete Link or button with Confirmation in MVC project and using actionLink
    Classic MVC Delete Link or button with Confirmation in project and using actionLink. you can use your mvc project.

---
+++ DeleteBtnWithConfirmBtn/DeleteBtnWithConfirmBtn/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeleteBtnWithConfirmBtn.Models;
+
+namespace DeleteBtnWithConfirmBtn.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        NorthwindEntities _db = new NorthwindEntities();
+
+        public ActionResult Index()
+        {
+            var allEmployees = _db.Employees.ToList();
+            return View(allEmployees);
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            Employee willDeleteEmployee = _db.Employees.Find(id);
+            _db.Employees.Remove(willDeleteEmployee);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
